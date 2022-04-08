2b2ec1d7e6eabbaf8b42abe7d8fda985e1933661
Samuel Santos <99606436+samuellsantos@users.noreply.github.com>
2022-04-08 18:45:22 -0300

    Treinando vetores

---
+++ Treinando JS/Variaveis Compostas/t1.js
@@ -0,0 +1,16 @@
+/*O que são variaveis compostaS?
+Basicamente são variáveis que podem armazenar mais de um valor dentro dela. Podendo ser chamados de Arrays ou vetores*/
+
+let n = [2, 1, 4, 3]
+n.push(5)// Adcionar mais um valor ao vetor
+n.sort()
+console.log(n)
+
+console.log(`O número 3 está na posição: ${n.indexOf(3)}`)
+
+
+
+
+
+//Tamanho do Array
+/*console.log(`O tamanho deste array é de: ${n.length}`)*/
