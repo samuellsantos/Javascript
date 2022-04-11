0531ca19c7b4dbfa43f24afe8cb5910f0ebc30af
Samuel Santos <99606436+samuellsantos@users.noreply.github.com>
2022-04-11 18:26:26 -0300

    trabalhando no exercicio

--- aula16ex/ex018/modelo/script.js
+++ aula16ex/ex018/modelo/script.js
@@ -1,7 +1,27 @@
 let n = document.getElementById('number')
+let r = document.getElementById('res')
+let valores = []
+let sel = document.getElementById('numbers')
+
+
 
 
 function adicionar(){
-    
+    if(n.value > 100){
+        alert('ERROR')
+    }else{
+        valores.push(n.value)
+        let val = document.createElement('option')
+        val.text = `Valor ${n.value} foi adicionado`
+        sel.appendChild(val)
+    }
+
+}
+
+
+function finalizar(){
+    valores.sort()
+    r.innerHTML = `Ao todo temos ${valores.length} números cadastrados <br>`
+
 }
 
