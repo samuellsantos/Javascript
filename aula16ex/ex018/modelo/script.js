ccf1fb66025bee9d10dad4d5898887127932aaaf
Samuel Santos <99606436+samuellsantos@users.noreply.github.com>
2022-04-11 10:08:25 -0300

    resolvendo exercicio

--- aula16ex/ex018/modelo/script.js
+++ aula16ex/ex018/modelo/script.js
@@ -1,29 +1,7 @@
-let num = document.getElementById('number')
-let lista = document.getElementById('flista')
-let res = document.getElementById('res')
-let valores = []
-
-function isNumero(n){
-    if (Number(n) >= 1 && Number(n) <= 100){
-        return true
-    }else{
-        return false
-    }
-}
-
-function inlista(n, l){
-    if (l.indexOf(Number(n)) != -1){
-        return true
-    }else{
-        return false
-    }
-}
+let n = document.getElementById('number')
 
 
 function adicionar(){
-    if(isNumero(num.value) && !inlista(num.value, valores)){
-
-    }else{
-        alert('Valor inválido ou já encontrado na lista.')
-    }
+    
 }
+
