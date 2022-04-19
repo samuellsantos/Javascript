db1790ea7529add084dfe19d5573d9e68ef2d6e1
Samuel Santos <99606436+samuellsantos@users.noreply.github.com>
2022-04-19 19:05:03 -0300

    Treinando...
    Treinando funções com parâmetros

---
+++ Treinando JS/Funções/Treinando Função/exemplo1/ex.js
@@ -0,0 +1,19 @@
+function quadrado(n){
+    let r = n * n 
+    document.writeln(`A potência de ${n} é: ${r} <br/>`)
+}
+let res = quadrado(2)
+
+
+function dobro(num){
+    let n = num + num
+    document.writeln(`O dobro de ${num} é: ${n} <br>`)
+}
+let resultado = dobro(10)
+
+function divisao(nm){
+    let r = nm / 2
+    document.write(`A divisão entre ${nm} e 2 é: ${r}`)
+}
+let nn = divisao(4)
+
