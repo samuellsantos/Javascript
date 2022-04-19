a4d00270b3637784f1c16b88dd7437043d146e8d
Samuel Santos <99606436+samuellsantos@users.noreply.github.com>
2022-04-19 19:23:21 -0300

    Aprendendo....
    Número aleatório entre 0 e 10

--- Treinando JS/Funções/Treinando Função/NumAleatório/script.js
+++ Treinando JS/Funções/Treinando Função/NumAleatório/script.js
@@ -1,6 +1,19 @@
-var button = document.getElementById('action')
+/* Entre 0 e 1 */
+
+/* var button = document.getElementById('action')
 button.addEventListener('click', aleatorio)
 function aleatorio(){
     var res = document.getElementById('res')
     res.innerHTML = Math.random()
+} */
+
+
+
+
+/* Entre 0 e 10 */
+var button = document.getElementById('action')
+button.addEventListener('click', aleatorio)
+function aleatorio(){
+    var res = document.getElementById('res')
+    res.innerHTML = Math.floor(10*Math.random())
 }
