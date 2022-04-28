08d907c29105b46eec385618fbd254373b36f802
Samuel Santos <99606436+samuellsantos@users.noreply.github.com>
2022-04-28 19:31:32 -0300

    Realizando exercícios de Condicionais

--- Treinando JS/Condicionais/exercicios/ex08/script.js
+++ Treinando JS/Condicionais/exercicios/ex08/script.js
@@ -1,15 +1,52 @@
 function script(){
-    var salario = parseInt(document.getElementById('n'))
+    var Salario = document.getElementById('n')
+    var salario = Number(Salario.value)
     var resultado = document.getElementById('res')
 
+    if(salario > 0){
+        /* SALARIOS ATÉ R$280,00 */
+        if(salario <= 280){
+            let percentual = '20%'
+            let valorAumento = (salario *20)/100
+            let total = salario + valorAumento
+            resultado.innerHTML = `Salário antes do reajuste: <strong>R$${salario}</strong> ;<br>`
+            resultado.innerHTML += `Percentual de aumento aplicado: <strong>${percentual}</strong>;<br>`
+            resultado.innerHTML += `Valor do aumento: <strong>R$${valorAumento}</strong>; <br>`
+            resultado.innerHTML += `O valor após o aumento é de: <strong>R$${total}</strong>;`
+        }
 
-    if(salario.value < 280){
-        var percentual = '20%'
-        var valorAumento = (salario.value *20)/100
-        var total = salario.value + valorAumento
-        resultado.innerHTML = `Salário antes do reajuste: ${salario.value}<br>`
-        resultado.innerHTML += `Percentual de aumento aplicado: ${percentual}<br>`
-        resultado.innerHTML += `Valor do aumento: ${valorAumento} <br>`
-        resultado.innerHTML += `O valor após o aumento é de: ${total}`
+        /* SALÁRIOS ENTRE 280 E 700 */
+        else if(salario > 280 && salario <= 700){
+            let percentual = '15%'
+            let valorAumento = (salario * 15)/100
+            let total = salario + valorAumento
+            resultado.innerHTML = `Salário antes do reajuste: <strong>R$${salario},00</strong> ;<br>`
+            resultado.innerHTML += `Percentual de aumento aplicado: <strong>${percentual}</strong>;<br>`
+            resultado.innerHTML += `Valor do aumento: <strong>R$${valorAumento}</strong>; <br>`
+            resultado.innerHTML += `O valor após o aumento é de: <strong>R$${total}</strong>;`
+            
+        }
+
+        /* SALÁRIOS ENTRE 700 E 1500 */
+        else if(salario > 700 && salario <= 1500){
+            let percentual = '10%'
+            let valorAumento = (salario * 10)/100
+            let total = salario + valorAumento
+            resultado.innerHTML = `Salário antes do reajuste: <strong>R$${salario}</strong> ;<br>`
+            resultado.innerHTML += `Percentual de aumento aplicado: <strong>${percentual}</strong>;<br>`
+            resultado.innerHTML += `Valor do aumento: <strong>R$${valorAumento}</strong>; <br>`
+            resultado.innerHTML += `O valor após o aumento é de: <strong>R$${total}</strong>;`
+        }
+        else if(salario > 1500){
+            let percentual = '5%'
+            let valorAumento = (salario * 5)/10
+            let total = salario + valorAumento
+            resultado.innerHTML = `Salário antes do reajuste: <strong>R$${salario}</strong> ;<br>`
+            resultado.innerHTML += `Percentual de aumento aplicado: <strong>${percentual}</strong>;<br>`
+            resultado.innerHTML += `Valor do aumento: <strong>R$${valorAumento}</strong>; <br>`
+            resultado.innerHTML += `O valor após o aumento é de: <strong>R$${total}</strong>;`
+        }
+    }else{
+        alert("[ERRO] Observe os seus dados e tente novamente")
     }
 }
