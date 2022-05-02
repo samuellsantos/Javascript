7f15a7aeba2928e934f1a6f6d4e8521d13fb29e2
Samuel Santos <99606436+samuellsantos@users.noreply.github.com>
2022-05-02 20:40:25 -0300

    Realizando Ex10 - Condicionais

--- Treinando JS/Condicionais/exercicios/ex10/script.js
+++ Treinando JS/Condicionais/exercicios/ex10/script.js
@@ -1,21 +1,21 @@
 function diaSemana(){
     var dia = document.getElementById('n').value
     var resultado = document.getElementById('res')
-
+    resultado.innerHTML = `Você digitou o número ${dia}, que corresponde a:  `
     if(dia == 1){
-        resultado.innerHTML = "<strong>DOMINGO</strong>"
+        resultado.innerHTML += "<strong>DOMINGO</strong>"
     }else if (dia == 2){
-        resultado.innerHTML = "<strong>SEGUNDA - FEIRA</strong>"
+        resultado.innerHTML += "<strong>SEGUNDA - FEIRA</strong>"
     }else if (dia == 3){
-        resultado.innerHTML = "<strong>TERÇA - FEIRA</strong>"
+        resultado.innerHTML += "<strong>TERÇA - FEIRA</strong>"
     }else if (dia == 4){
-        resultado.innerHTML = "<strong>QUARTA - FEIRA</strong>"
+        resultado.innerHTML += "<strong>QUARTA - FEIRA</strong>"
     }else if (dia == 5){
-        resultado.innerHTML = "<strong>QUINTA - FEIRA</strong>"
+        resultado.innerHTML += "<strong>QUINTA - FEIRA</strong>"
     }else if(dia == 6){
-        resultado.innerHTML = "<strong>SEXTA - FEIRA</strong>"
+        resultado.innerHTML += "<strong>SEXTA - FEIRA</strong>"
     }else if(dia == 7){
-        resultado.innerHTML = "<strong>SÁBADO</strong>"
+        resultado.innerHTML += "<strong>SÁBADO</strong>"
     }else{
         alert("[ERRO] Digite um valor válido!")
         resultado.innerHTML = ""
