67aff78204226477422bf91887aba4799953d46e
Samuel Santos <99606436+samuellsantos@users.noreply.github.com>
2022-05-02 20:36:47 -0300

    Realizando exercício 10 - Condicionais

---
+++ Treinando JS/Condicionais/exercicios/ex10/script.js
@@ -0,0 +1,24 @@
+function diaSemana(){
+    var dia = document.getElementById('n').value
+    var resultado = document.getElementById('res')
+
+    if(dia == 1){
+        resultado.innerHTML = "<strong>DOMINGO</strong>"
+    }else if (dia == 2){
+        resultado.innerHTML = "<strong>SEGUNDA - FEIRA</strong>"
+    }else if (dia == 3){
+        resultado.innerHTML = "<strong>TERÇA - FEIRA</strong>"
+    }else if (dia == 4){
+        resultado.innerHTML = "<strong>QUARTA - FEIRA</strong>"
+    }else if (dia == 5){
+        resultado.innerHTML = "<strong>QUINTA - FEIRA</strong>"
+    }else if(dia == 6){
+        resultado.innerHTML = "<strong>SEXTA - FEIRA</strong>"
+    }else if(dia == 7){
+        resultado.innerHTML = "<strong>SÁBADO</strong>"
+    }else{
+        alert("[ERRO] Digite um valor válido!")
+        resultado.innerHTML = ""
+    }
+
+}
