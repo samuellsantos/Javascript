5476923493c97ca1e97ff37d9f24494294b125d9
Samuel Santos <99606436+samuellsantos@users.noreply.github.com>
2022-05-04 20:27:49 -0300

    FOR IN - FOR OF

---
+++ Treinando JS/FOR IN - FOR OF/for.js
@@ -0,0 +1,11 @@
+/* const aluno = {nome: "Samuel", sobrenome: "Santos"}
+
+for (var dado in aluno){
+    console.log(aluno[dado])
+} */
+
+
+var notas = [10, 7, 3, 8]
+for(var n of notas){
+    console.log(n)
+}
