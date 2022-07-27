f97db17e3dbee9d821400f20a33865b568672bcf
Samuel Santos <samuka0102128@gmail.com>
2022-07-27 18:19:49 -0300

    Criando um Modal com JS

---
+++ Projetos/Modal/script.js
@@ -0,0 +1,16 @@
+const openModalButton = document.querySelector('#open-modal')
+const closeModalButton = document.querySelector('#close-modal')
+const modal = document.querySelector('#modal')
+const fade = document.querySelector('#fade')
+
+
+const toggleModal = () => {
+    modal.classList.toggle("hide")
+    fade.classList.toggle("hide")
+}
+[openModalButton, closeModalButton, fade].forEach((el) => {
+    el.addEventListener("click", () => toggleModal());
+  });
+/* [openModalButton, closeModalButton, fade].forEach((el) => {
+    el.addEventListener('click', () => alert('teste'))
+}) */
